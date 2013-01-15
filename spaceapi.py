ba10b2875fa2f8a3c68b2cf9e366c052a3f0b127
Trever Fischer <tdfischer@fedoraproject.org>
2013-01-15 18:03:38 -0500

    crash--

--- spaceapi.py
+++ spaceapi.py
@@ -43,7 +43,7 @@ class API(object):
         self._timeout = timeout
 
     def __repr__(self):
-        return "API('%s')"%(self._url)
+        return "API('%s')"%(self._url.geturl())
 
     def __str__(self):
         return repr(self)
